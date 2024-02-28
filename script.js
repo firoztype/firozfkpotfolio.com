cffbb741a3c8bfc08ef84bb775ff4622081f4332
firoztype <98393712+firoztype@users.noreply.github.com>
2024-02-28 20:17:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+/*=====sticky slections active line=====
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+==*/
+
+
+
+/*=====sticky slections active line=====*/
+/*--let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+
+            });
+        };
+    });
+
+
+/*=====sticky navbar=====
+let header = document.querySelector('.header');
+
+header.classList.toggle('sticky', window.scrollY > 100);
+};
+
+/*=====slider=====*/
+
+var swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    spaceBetween: 50,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
